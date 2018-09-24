478b1f24020ece9dc8ba36bd4700d1355b72af2f
Bérengère COLSON <berengere.colson@capgemini.com>
2018-09-24 13:48:07 +0200

    Init project:

---
+++ GiftsProject/GiftsProject/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GiftsProject.Models;
+
+namespace GiftsProject.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
